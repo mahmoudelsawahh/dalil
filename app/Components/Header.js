58574ebe3fba77bbe3cf4ae2ce40bdd5d826da93
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-25 15:31:45 +0300

    s

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -83,9 +83,7 @@ const Header = ({ Categories }) => {
         className={styles.img_container}
         // style={{ backgroundImage: `url(${loaded})` }}
       >
-        <LazyLoadComponent>
         <Image src={mainBg} alt="background-deltawy-image" layout="fill" objectFit="cover" objectPosition="center" priority/>
-        </LazyLoadComponent>
         <div className={styles.Header_content}>
           <div className={styles.text_contet}>
             <div className={styles.warpper}>
